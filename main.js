4bb879ce6b76a63c234360f260a39a4e68121e53
Te Nguyen <manhte231@gmail.com>
2020-07-25 23:58:28 +0700

    init code

---
+++ main.js
@@ -0,0 +1,50 @@
+// Imports the Google Cloud client library
+const {PubSub} = require('@google-cloud/pubsub');
+
+const config = {
+    projectId: 'mediaone-thp',// Your Google Cloud Platform project ID
+    topicName: 'projects/mediaone-thp/topics/stagging-common-log', // Name for the new topic to create
+    subscriptionName: "projects/mediaone-thp/subscriptions/thp-client"
+}
+
+const err = {
+    pubsub_not_found: "pubsub_not_found"
+}
+
+function connect(projectId) {
+    const pubsub = new PubSub({projectId})
+    return pubsub
+}
+
+// @cb = {}
+function onStreamMessage(psClient, subscriptionName, cb) {
+    if(!psClient) {
+        return new Error(err.pubsub_not_found)
+    }
+    const subscription = psClient.subscription(subscriptionName)
+
+    const messageHandler = function(message) {
+        const payload = {
+            message: message,
+        }
+        cb(payload)
+        message.ack()
+    }
+
+    subscription.on('message', messageHandler)
+    return null
+}
+
+function main() {
+    // make client
+    const ps = connect(config.projectId)
+    let err = onStreamMessage(ps, config.subscriptionName,  function({message, ack}) {
+        console.log(message.id, message.data.toString())
+        // ack()
+    })
+    if(!err) {
+        console.log('[err]: ', err)
+    }
+}
+
+main()
